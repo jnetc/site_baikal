92509f2f123d7d258eaf90ebbd89471b69a4753b
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2019-07-13 17:43:42 +0300

    004

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,7 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-
-// import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
@@ -9,6 +7,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <i className="icon-fish" style={{ fontSize: '2em', color: '#856'}}></i>
     <Link to="/">Go back to the homepage</Link>
   </>
 )
