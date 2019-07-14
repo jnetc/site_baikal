38382056225f0b197b5e6ca9a1ee5345425a441e
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2019-07-14 16:30:26 +0300

    003

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -9,7 +9,7 @@ export default class Head extends React.Component {
         <Helmet>          
           <meta charSet="utf-8" />
           <title>Baikal site</title>          
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
+          {/* <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/> */}
           {/* <link href="/static/icons/fontello.css" rel="stylesheet" type="text/css"/> */}
         </Helmet>
       </div>
