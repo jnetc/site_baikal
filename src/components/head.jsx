a00c7cbf72b40ea19cee70dd85b2e160fd5060b3
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2019-07-14 15:10:04 +0300

    insert

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+// import '../../static/icons/fontello.css'
 
 export default class Head extends React.Component {
   render() {
@@ -9,7 +10,7 @@ export default class Head extends React.Component {
           <meta charSet="utf-8" />
           <title>Baikal site</title>          
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
-          <link href="/static/icons/fontello.css" rel="stylesheet" type="text/css"/>
+          {/* <link href="/static/icons/fontello.css" rel="stylesheet" type="text/css"/> */}
         </Helmet>
       </div>
     )
