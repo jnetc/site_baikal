1004e3a8e0744e4436c8ed995772016a956ef825
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2019-07-26 22:15:00 +0300

    4 попытка

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import TransitionPage from '../components/transition-page/'
 import { graphql } from 'gatsby'
+import { CSSTransition } from 'react-transition-group'
 
 import Background from '../components/background/'
 import Navigator from '../components/navigation'
 
 const Layout = (props) => {
-
+  // console.log(props);
   console.log(props);
+
+  
+  const path =  props.location.pathname.split('/').splice(1, 1)[0];
+  console.log(path);
+  
   return (
     <React.Fragment>
       <TransitionPage props={ props }>
         { props.children }
       </TransitionPage>
       <Background/>
-      { props.data.allContentfulVodkaProduct !== undefined &&  
-         <Navigator props={ props.data.allContentfulVodkaProduct.edges }/>}
+      { path === "vodka" &&  
+         <Navigator 
+          props={ props.data.allContentfulVodkaProduct.edges }/>}
+      
     </React.Fragment>
   )
 }
