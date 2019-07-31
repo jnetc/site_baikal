2e18d4e60c6742a1891a9a5d6c546a03e01617db
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2019-07-31 15:45:11 +0300

    remove tooltip

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -29,34 +29,27 @@ const Contacts = () => {
   return (
     <ul id="info-block" style={{ zIndex: "500"}}>
       <li className="info-box">
-        <a href={`mailto: ${ email }`}><span className="icon-email" title={ email }>
-          <p className="tooltip">{ email }</p>
-        </span></a>
+        <a href={`mailto: ${ email }`}>
+          <span className="icon-email" title={ email }></span>
+        </a>
       </li>
       { facebook !== undefined && 
         <li className="info-box">
-          <a href={ facebook }><span className="icon-face" title={ cutFacebook }>
-          <p className="tooltip">{ cutFacebook }</p>
-          </span></a>
+          <a href={ facebook }>
+            <span className="icon-face" title={ cutFacebook }></span>
+          </a>
         </li> }
       
       <li className="info-box">
-        <a href={ geo }><span className="icon-geo" title={ address.address }>
-        <p className="tooltip">{ address.address }</p>
-        </span></a>
+        <a href={ geo }>
+          <span className="icon-geo" title={ address.address }></span>
+        </a>
       </li>
       <li className="info-box">
-        <a href={`tel: ${ tel }`}><span className="icon-phone" title={ tel }>
-        <div className="tooltip">{ tel }</div>
-        </span></a>
+        <a href={`tel: ${ tel }`}>
+          <span className="icon-phone" title={ tel }></span>
+        </a>
       </li>
-      { info !== undefined && 
-        <li className="info-box">
-          <span className="icon-info">
-            <p className="tooltip">{ info.info }</p>
-          </span>
-        </li> }
-      
     </ul>
   )
 }
